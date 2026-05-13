a934831c36580558df3ae563dac5a128f57ae2c4
sumedha-17 <pothukanisumedha@gmail.com>
2026-05-13 15:30:17 +0530

    BankersAlgorithm.c

---
+++ BankersAlgorithm.c
@@ -0,0 +1,72 @@
+3. Write a C program to simulate Bankers Algorithm for Deadlock Avoidance and Prevention
+
+
+🏦 Banker’s Algorithm – C Program
+#include <stdio.h>
+
+int main() {
+    int n, m, i, j, k;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    printf("Enter number of resources: ");
+    scanf("%d", &m);
+
+    int alloc[n][m], max[n][m], need[n][m];
+    int avail[m], finish[n], safeSeq[n];
+
+    printf("Enter Allocation Matrix:\n");
+    for (i = 0; i < n; i++)
+        for (j = 0; j < m; j++)
+            scanf("%d", &alloc[i][j]);
+
+    printf("Enter Maximum Matrix:\n");
+    for (i = 0; i < n; i++)
+        for (j = 0; j < m; j++)
+            scanf("%d", &max[i][j]);
+
+    printf("Enter Available Resources:\n");
+    for (i = 0; i < m; i++)
+        scanf("%d", &avail[i]);
+
+    // Calculate Need Matrix
+    for (i = 0; i < n; i++)
+        for (j = 0; j < m; j++)
+            need[i][j] = max[i][j] - alloc[i][j];
+
+    for (i = 0; i < n; i++)
+        finish[i] = 0;
+
+    int count = 0;
+    while (count < n) {
+        int found = 0;
+        for (i = 0; i < n; i++) {
+            if (!finish[i]) {
+                for (j = 0; j < m; j++)
+                    if (need[i][j] > avail[j])
+                        break;
+
+                if (j == m) {
+                    for (k = 0; k < m; k++)
+                        avail[k] += alloc[i][k];
+
+                    safeSeq[count++] = i;
+                    finish[i] = 1;
+                    found = 1;
+                }
+            }
+        }
+
+        if (!found) {
+            printf("\nSystem is NOT in safe state\n");
+            return 0;
+        }
+    }
+
+    printf("\nSystem is in SAFE state\nSafe Sequence: ");
+    for (i = 0; i < n; i++)
+        printf("P%d ", safeSeq[i]);
+
+    return 0;
+}
